d6931eeca7d2c77a60d0ba48d12862cdde2ddba8
Valentin Sushkov <me@vsushkov.com>
2020-10-22 18:14:11 +0300

    Add the monster killa game

---
+++ 3/assets/scripts/vendor.js
@@ -0,0 +1,44 @@
+const monsterHealthBar = document.getElementById('monster-health');
+const playerHealthBar = document.getElementById('player-health');
+const bonusLifeEl = document.getElementById('bonus-life');
+
+const attackBtn = document.getElementById('attack-btn');
+const strongAttackBtn = document.getElementById('strong-attack-btn');
+const healBtn = document.getElementById('heal-btn');
+const logBtn = document.getElementById('log-btn');
+
+function adjustHealthBars(maxLife) {
+  monsterHealthBar.max = maxLife;
+  monsterHealthBar.value = maxLife;
+  playerHealthBar.max = maxLife;
+  playerHealthBar.value = maxLife;
+}
+
+function applyMonsterDamage(damage) {
+  const appliedDamage = Math.random() * damage;
+  monsterHealthBar.value = +monsterHealthBar.value - appliedDamage;
+  return appliedDamage;
+}
+
+function applyPlayerDamage(damage) {
+  const appliedDamage = Math.random() * damage;
+  playerHealthBar.value = +playerHealthBar.value - appliedDamage;
+  return appliedDamage;
+}
+
+function increasePlayerHealth(healValue) {
+  playerHealthBar.value = +playerHealthBar.value + healValue;
+}
+
+function resetGame(value) {
+  playerHealthBar.value = value;
+  monsterHealthBar.value = value;
+}
+
+function removeBonusLife() {
+  bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+}
+
+function setPlayerHealth(health) {
+  playerHealthBar.value = health;
+}
